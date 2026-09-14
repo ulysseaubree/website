a10f97fc6a164b327fe3a79775f2d7b6961083f8
ULYSSE <ulysse.aubree@gmail.com>
2026-09-14 17:02:42 +0200

    [Del] sup activity

--- mon-portfolio/src/App.jsx
+++ mon-portfolio/src/App.jsx
@@ -2,7 +2,6 @@ import Navbar from './components/ui/Navbar.jsx'
 import Hero from './components/sections/Hero.jsx'
 import PourQui from './components/sections/PourQui.jsx'
 import QuiSommesNous from './components/sections/QuiSommesNous.jsx'
-import Actualites from './components/sections/Actualites.jsx'
 import NotreOffre from './components/sections/NotreOffre.jsx'
 import Contact from './components/sections/Contact.jsx'
 import Footer from './components/ui/Footer.jsx'
@@ -13,7 +12,6 @@ export default function App() {
       <Navbar />
       <main>
         <Hero />
-        <Actualites />
         <PourQui />
         <QuiSommesNous />
         <NotreOffre />
